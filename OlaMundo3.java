6eb9156d825d3bb8850f17bfd2e26725b4568fc5
Marcos Costa <marcosoliveira3@aluno.uema.br>
2020-10-01 19:06:31 -0300

    lab02- print(1º e 2º), comentarios e pacotes

---
+++ OlaMundo3.java
@@ -0,0 +1,14 @@
+public class OlaMundo3 {
+    /*
+    tipo de comentario em bloco
+    essa eh a primeira linha de cometario
+    essa eh a segunda linha de cometario
+    */
+    public static void main(String[] args) {
+    //System.out.println("ola mundo") -> inativando linha de comando
+    // este eh um comentario independente
+    System.out.println("ola mundo um"); // comentario lateral
+    // System.out.println("ola mundo dois");
+    System.out.println(/* "ola mundo tres"*/ "esse eh um texto diferente");
+    }
+    }
---
+++ Ola_Mundo2.java
@@ -0,0 +1,10 @@
+public class Ola_Mundo2 {
+	public static void main(String[] args){
+		System.out.println("ola, Mundo!");
+		System.out.println( "ola, mundo!");
+		System.out.println(    "ola, mundo!");
+		System.out.println(
+			"ola, mundo"
+		);
+	}
+}
