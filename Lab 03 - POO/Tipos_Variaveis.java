536d1f1f592611ed1d8175e66298d0143585e257
marcos_costa <marcosoliveira3@aluno.uema.br>
2020-10-07 16:42:56 -0300

    usando variaveis

--- Lab 03 - POO/Tipos_Variaveis.java
+++ Lab 03 - POO/Tipos_Variaveis.java
@@ -1,19 +1,34 @@
-
-/*Lab03- variaveis, tipos de dados e operadoresmatemáticos
+/*Lab03- variaveis, tipos de dados e operadores matemáticos
   @author : Marcos Costa Oliveira
   Engenharia da Computação - UEMA
 */
 
 public class Tipos_Variaveis{
-    public static void name(String[] args) {
+   public static void main(String[] args){
          
-        //1º Declaraçao de uma variavel inteira
+        /*
+        * DECLARANDO E NOMEANDO VARIAVEIS
+        */
 
         int ValorFinal; // palavra composta
         int valor;      // palavra simples
 
         // apenas letras e numeros sao utilizados 
 
-        
+        /*
+        * USANDO VARIAVEIS
+        */
+        //System.out.println(ValorFinal); //ERROR-> variavel sem valor atribuido
+
+        valor = 50;       // atribuição de valores
+        ValorFinal = 120;
+        System.out.println(String.format("%s , %s", valor ,ValorFinal)); // 50, 120
+
+        valor = ValorFinal;
+        System.out.println(String.format("%s , %s", valor ,ValorFinal)); // 120, 120
+
+
+
+
     }
 }
