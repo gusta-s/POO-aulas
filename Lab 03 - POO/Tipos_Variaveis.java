7bab7299273b3a6b3ed4c20a268262ade7b80dd6
marcos_costa <marcosoliveira3@aluno.uema.br>
2020-10-07 16:28:54 -0300

    aula03

---
+++ Lab 03 - POO/Tipos_Variaveis.java
@@ -0,0 +1,19 @@
+
+/*Lab03- variaveis, tipos de dados e operadoresmatemáticos
+  @author : Marcos Costa Oliveira
+  Engenharia da Computação - UEMA
+*/
+
+public class Tipos_Variaveis{
+    public static void name(String[] args) {
+         
+        //1º Declaraçao de uma variavel inteira
+
+        int ValorFinal; // palavra composta
+        int valor;      // palavra simples
+
+        // apenas letras e numeros sao utilizados 
+
+        
+    }
+}
