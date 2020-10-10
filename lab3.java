bec5650a9b0b1016ca7714f7b157414cb043fd04
DigianS <lucassantos24@aluno.uema.br>
2020-10-10 11:23:28 -0300

    Lab3

---
+++ lab3.java
@@ -0,0 +1,42 @@
+/**
+ * @author Lucas Digian 
+ */
+
+package lab3;
+
+public class Variaveis2 {
+	public static void main(String[] args) {
+		int var;
+
+		var = 30;
+		System.out.println(car); // 30
+		
+		int outVar = 50;
+		System.out.println(outVar); //50
+		
+		var = outVar;
+		System.out.println(var); //  50
+		
+		outVar = 20;
+		System.out.println(outVar); // 20
+		
+		Integer variavel2; // tipos primitivo de variavel
+		variavel2 = null;
+		System.out.println(variavel2); // console: null
+		
+		/*
+		 * TIPOS DE VARIÁVEIS
+		 */
+		byte let = 26;
+		Byte let2 = 26;
+		System.out.println(let + " " + let2);
+
+		letras2 = null;
+		
+		short anoAtual = 2020;
+		long numeroLong = 216545154154154L; // o "L" deve ser posto no final do numero para indicar que eh do tipo long
+		float nota = 8.5f; // "f" deve ser posto no final para indicar que eh float
+		double numeroDouble = 0.00255050500450d; // "d" deve ser posto no final para indicar que eh double
+		
+    }
+}
