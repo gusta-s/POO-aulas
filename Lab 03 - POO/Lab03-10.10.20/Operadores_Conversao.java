c9cf8aa460c8a3411dd7da9ea931d80eacedebeb
marcos_costa <marcosoliveira3@aluno.uema.br>
2020-10-15 17:48:13 -0300

    Precedencia de operadores

--- Lab 03 - POO/Lab03-10.10.20/Operadores_Conversao.java
+++ Lab 03 - POO/Lab03-10.10.20/Operadores_Conversao.java
@@ -14,22 +14,46 @@ public class Operadores_Conversao{
         
         int numb = 15;
         numb *= 2;
-        System.out.println(numb); //console-> 30
+        System.out.println(numb); //console = 30
 
         numb -= 5;
-        System.out.println(numb); //console -> 25
+        System.out.println(numb); //console = 25
 
         int resultado = 180;
-        System.out.println(resultado); //console -> 180
+        System.out.println(resultado); //console = 180
 
         int numb1 = 6;
         resultado /= numb * numb1; //console 180/(6*15) = 2
-        System.out.println(resultado); // console -> 2
+        System.out.println(resultado); // console = 2
         
+        /*
+        * PRECEDENCIA DE OPERADORES
+        *    pós-fixados> pré-fixados> multiplicativos> aditivos
+        *    precedencia pode ser substituida por parenteses
+        */
 
+        int valor1 = 21;
+        int valor2 = 6;
+        int valor3 = 3;
+        int valor4 = 1;
 
+        int resultadoA = valor1 - valor2 / valor3; //ordem de precedencia
+        System.out.println(resultadoA); //console = 19
 
+        int resultadoB = valor1 * valor2 / valor3; //igual precedencia: esquerda -> direita
+        System.out.println(resultadoB); //console = 42
 
+        int resultadoC = (valor1 - valor2) / valor3; //precedencia pode ser substituida por parenteses
+        System.out.println(resultadoC); //console = 5
+
+        int resultadoD = valor1 / valor3 * valor4 + valor2; // (21/(3*1)) + 6
+        System.out.println(resultadoD); //console = 13
+
+        int resultadoE = valor1 / (valor3 * (valor4 + valor2)); // 21/ (3*(1+6))
+        System.out.println(resultadoE); //console = 1
+
+        int resultadoF = valor4 + --valor3 * valor2++; // 1+(2 * 6)
+        System.out.println(resultadoF);//13
 
 
 
